00abc3dab65e092bd702dcd9478ecf442422f451
feniksg <doroxincavely@gmail.com>
2024-02-09 20:20:57 +0300

    add update function

--- src/database.py
+++ src/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import URL, text, create_engine
 from sqlalchemy.orm import Session,sessionmaker
 from config import settings
 from models import AbstractModel, ProductModel
+from typing import List
 
 engine = create_engine(
     url = settings.DATABASE_URL_psycopg,
@@ -21,14 +22,44 @@ def create_tables():
     AbstractModel.metadata.drop_all(engine)
     AbstractModel.metadata.create_all(engine)
 
-def insert_items(list):
-    for item in list:
-        session.add(item)
-    session.commit()
-
-def item_from_ms(body):
-    ...
-
+def insert_items(list:List[ProductModel]):
+    try:
+        for item in list:
+            check = session.query(ProductModel).filter(ProductModel.ms_code == item.ms_code).all()
+            if check:
+                check = check[0]
+                if check.name != item.name:
+                    check.name = item.name
+                if check.retail_price != item.retail_price:
+                    check.retail_price = item.retail_price
+                if check.mt10 != item.mt10:
+                    check.mt10 = item.mt10
+                if check.image != item.image:
+                    check.image = item.image
+                if check.nicotine_strength != item.nicotine_strength:
+                    check.nicotine_strength = item.nicotine_strength
+                if check.taste != item.taste:
+                    check.taste = item.taste
+                if check.snus_type != item.snus_type:
+                    check.snus_type = item.snus_type
+                if check.brand != item.brand:
+                    check.brand = item.brand
+                if check.currency != item.currency:
+                    check.currency = item.currency
+                if check.sale_price != item.sale_price:
+                    check.sale_price = item.sale_price
+                if check.is_sale != item.is_sale:
+                    check.is_sale = item.is_sale
+                if check.is_popular != item.is_popular:
+                    check.is_popular = item.is_popular
+                if check.search_name != item.search_name:
+                    check.search_name = item.search_name
+            else:
+                session.add(item)
+        session.commit()
+    except Exception as e:
+        session.rollback()
+   
 def get_item(id):
     item = session.query(ProductModel).filter(ProductModel.id == id).first()
     if item:
@@ -73,6 +104,6 @@ def get_items(brand = None, nicotine_strength = None, taste = None, snus_type = 
           
 if __name__ == '__main__':
     # insert_items([CategoryModel(name="Cat's and Crabs")])
-    get_items(search="mint")
+    insert_items([ProductModel(ms_code='00001',name='Тестовое  имя',retail_price= 200.0,image='00001.png', mt10= 180.0,nicotine_strength= 20.0, taste='MINT',snus_type= "1",brand= "1",currency= "THB",sale_price= 0,is_sale= False, is_popular=False, search_name= 'dsfsdf')])
     # create_tables()
     # get_categories()
